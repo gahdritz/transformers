4af010b2d4482aa24cea67633c918828814b6001
Gustaf Ahdritz <gahdritz@gmail.com>
2024-05-30 17:15:02 -0700

    Delay norm in Llama HS computation

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -983,12 +983,12 @@ class LlamaModel(LlamaPreTrainedModel):
             if output_attentions:
                 all_self_attns += (layer_outputs[1],)
 
-        hidden_states = self.norm(hidden_states)
-
         # add hidden states from the last decoder layer
         if output_hidden_states:
             all_hidden_states += (hidden_states,)
 
+        hidden_states = self.norm(hidden_states)
+        
         next_cache = next_decoder_cache if use_cache else None
         if return_legacy_cache:
             next_cache = next_cache.to_legacy_cache()
